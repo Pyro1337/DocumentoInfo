2c3ef294e958215d2cc5e6324171c547669f2df0
Ivan Sanchez <Ivan Sanchez@TECNOLOGIA002>
2023-06-22 11:35:22 -0400

    Add project files.

---
+++ InformacionDocumento/InformacionDocumento.cs
@@ -0,0 +1,63 @@
+﻿using iText;
+using iText.Kernel.Pdf;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace InformacionDocumento
+{
+    public class InfoDocument
+    {
+        string rutaCarpeta;
+        //inicializamos el constructor con la ruta de la carpeta auxiliar que utilizaremos.
+        public InfoDocument(string rutaCarpeta) {
+            this.rutaCarpeta = rutaCarpeta; 
+        }
+        public int obtenerCantPaginas(byte[] bytes , string extension)
+        {
+            if (extension == "pdf") //si la extension es .pdf 
+            {
+                if (!Directory.Exists(rutaCarpeta))
+                {
+                    Directory.CreateDirectory(rutaCarpeta);//sino existe el directorio carpetaPrueba lo creara.
+
+                }
+                string rutaArchivoNuevo = Path.Combine(rutaCarpeta, "archivoaux." + extension);//destino y nombre del archivoPrueba acoplamos con la extension en este caso pdf
+                File.WriteAllBytes(rutaArchivoNuevo, bytes);//convierte byte a formato PDF lleva como parametro
+                var pdfDocument = new PdfDocument(reader: new PdfReader(rutaArchivoNuevo)); //usamos el pdfreader con la nueva ruta
+                int cantPaginas = pdfDocument.GetNumberOfPages();//extraemos la cantidad de paginas con la funcion que contiene dicha libreria
+                pdfDocument.Close();//cerramos el pdfDocument para limpiar el buffer y posteriormente
+                eliminarArchivoCarpeta(rutaArchivoNuevo);
+                return cantPaginas;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public double obtenerTamanioArchivo(byte[] bytes , string extension)
+        {
+            if (!Directory.Exists(rutaCarpeta))
+            {
+                Directory.CreateDirectory(rutaCarpeta);//sino existe el directorio carpetaPrueba lo creara.
+
+            }
+            string rutaArchivoNuevo = Path.Combine(rutaCarpeta, "archivoaux." + extension);//destino y nombre del archivoPrueba acoplamos con la extension en este caso pdf
+            File.WriteAllBytes(rutaArchivoNuevo, bytes);//convierte byte a formato correspondiente
+            FileInfo fi = new FileInfo(rutaArchivoNuevo);
+            string tamaniokb = fi.Length.ToString();
+            double resultadoKB = double.Parse(tamaniokb)/1000;
+            eliminarArchivoCarpeta(rutaArchivoNuevo);
+            return resultadoKB;
+            
+        }            
+        private void eliminarArchivoCarpeta(string rutaArchivo)
+        {
+            if (Directory.Exists(rutaCarpeta))
+            {
+                File.Delete(rutaArchivo);//en caso de existir dicho directorio elimina el contenido
+                Directory.Delete(rutaCarpeta);//posteriormente elimina la carpeta
+
+            }
+        }
+
+    }
+}
---
+++ TestDocumentInformation/Program.cs
@@ -0,0 +1,10 @@
+﻿using InformacionDocumento;
+string rutaCarpeta = @"C:\carpetaPrueba"; //directorio de prueba
+InfoDocument infodoc = new InfoDocument(rutaCarpeta);
+string rutaPrueba = @"C:\rutaPruebaPaginas\redes.pdf";
+byte[] bytes = File.ReadAllBytes(rutaPrueba);
+string extension = "pdf";
+int resultado = infodoc.obtenerCantPaginas(bytes,extension);
+double tamanio = infodoc.obtenerTamanioArchivo(bytes,extension);
+Console.WriteLine("la cantidad de paginas es : "+resultado);
+Console.WriteLine("el tamanio del archivo es: "+tamanio);
